0bcb94ea43e18c766c2c1f85e01ab0c858cc55e8
Jeonghun Bu <karkata@gmail.com>
2019-07-12 10:17:07 +0900

    개발 환경 교체로 인한 gulfile 및 package-lock 파일 수정

--- gulpfile.js
+++ gulpfile.js
@@ -3,46 +3,46 @@ const eslint = require("gulp-eslint");
 const terser = require("gulp-terser");
 
 let paths = {
-	js: "public/src",
-	example: "public/example",
-	dist: "public/dist"
+    js: "public/src",
+    example: "public/example",
+    dist: "public/dist"
 };
 
 function syntax(cb) {
-	gulp.src(paths.js + "/*.js")
-		.pipe(eslint({ envs: [ "browser" ] }))
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-	cb();
+    gulp.src(paths.js + "/*.js")
+        .pipe(eslint({ envs: [ "browser" ] }))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+    cb();
 }
 
 function syntaxForTest(cb) {
-	gulp.src(paths.js + "/*.js")
-		.pipe(eslint({ 
-			envs: [ "browser" ], 
-			// console 사용된 코드를 무시한다.
-			rules: { "no-console": "off" } 
-		}))
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-	cb();
+    gulp.src(paths.js + "/*.js")
+        .pipe(eslint({ 
+            envs: [ "browser" ], 
+            // console 사용된 코드를 무시한다.
+            rules: { "no-console": "off" } 
+        }))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+    cb();
 }
 
 function minify(cb) {
-	gulp.src(paths.js + "/*.js")
-		.pipe(terser())
-		.pipe(gulp.dest(paths.dist + "/"));
-	cb();
+    gulp.src(paths.js + "/*.js")
+        .pipe(terser())
+        .pipe(gulp.dest(paths.dist + "/"));
+    cb();
 }
 
 function copySourceForTest(cb) {
-	gulp.src(paths.js + "/*.js").pipe(gulp.dest("/www/web/tsart/js/"));
-	cb();
+    gulp.src(paths.js + "/*.js").pipe(gulp.dest("/var/www/html/tsart/js/"));
+    cb();
 }
 
 function copyExampleForTest(cb) {
-	gulp.src(paths.example + "/*.html").pipe(gulp.dest("/www/web/tsart/"));
-	cb();
+    gulp.src(paths.example + "/*.html").pipe(gulp.dest("/var/www/html/tsart/"));
+    cb();
 }
 
 exports.build = gulp.series(syntax, minify);
